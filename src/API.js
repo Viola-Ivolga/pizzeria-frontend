00d32b9b0ce8dd5c2c22cbba63918f196684e5a1
Viola-Ivolga <98428056+Viola-Ivolga@users.noreply.github.com>
2022-11-02 20:24:43 +0000

    css updated & API.JS

--- src/API.js
+++ src/API.js
@@ -10,146 +10,147 @@ let baseURL;
 baseURL = 'https://pizzeria-backend.viola-ivolga.repl.co/'
 
 const api = axios.create({
-    baseURL: baseURL,
-    headers: {
-        'Content-Type': 'application/json',
-   
-    }
+  baseURL: baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+
+  }
 });
 
 api.interceptors.request.use(
-    config => {
-        if (localStorage.getItem(LOGIN_USER_KEY)) {
-            config.headers.common['Authorization'] = JSON.parse(localStorage.getItem(LOGIN_USER_KEY)).token;
-        }
-
-        return config;
-    },
-    err => {
-        console.error(err);
+  config => {
+    if (localStorage.getItem(LOGIN_USER_KEY)) {
+      config.headers.common['Authorization'] = JSON.parse(localStorage.getItem(LOGIN_USER_KEY)).token;
     }
+
+    return config;
+  },
+  err => {
+    console.error(err);
+  }
 );
 
 export default class API {
-  
-    signUp = async(user_name,password,email) =>{
-        const signup = await api
-        .post("signup/",{
-            user_name:user_name, password:password, email:email
-        })
-        .then((response)=>{
-            return response.data
-        })
 
-        .catch((error)=>{
-            throw new Error(error)
-        })
+  signUp = async (user_name, email, password) => {
+    const signup = await api
+      .post("signup/", {
+        user_name: user_name, password: password, email: email
+      })
+      .then((response) => {
+        return response.data
+      })
 
-    return signup 
-    }
+      .catch((error) => {
+        throw new Error(error)
+      })
+
+    return signup
+  }
 
 
 
-    signIn = async(email, password) =>{
-        const signin = await api
-        .post("signin/",{
-             password:password, email:email
-        })
-        .then((response)=>{
-            return response.data
-        })
+  signIn = async (email, password) => {
+    const signin = await api
+      .post("signin/", {
+        password: password, email: email
+      })
+      .then((response) => {
+        return response.data
+      })
 
-        .catch((error)=>{
-            throw new Error(error)
-        })
-        
+      .catch((error) => {
+        throw new Error(error)
+      })
+
     return signin
-    }
+  }
 
 
-    getUser = async() =>{
-        const user = await api
-        const url=''
-        .get(url)
-       
-        .then((response)=>{
-            return response.data
-        })
+  getUser = async () => {
+    const user = await api
+    const url = ''
+      .get(url)
 
-        .catch((error)=>{
-            throw new Error(error)
-        })
-        
+      .then((response) => {
+        return response.data
+      })
+
+      .catch((error) => {
+        throw new Error(error)
+      })
+
     return user
+  }
+  getItems = async params => {
+    try {
+      const response = await api.get('items/', { params });
+      return response.data;
+    } catch (error) {
+      throw new Error(error);
     }
-        getItems = async params => {
-        try {
-            const response = await api.get('items/', { params });
-            return response.data;
-        } catch (error) {
-            throw new Error(error);
-        }
-    };
+  };
 
 
-    getCart = async () => {
-        try {
-            const response = await api.get('carts/');
-            return response.data;
-        } catch (error) {
-            throw new Error(error);
-        }
-    };
-    addCart = async params => {
-        try {
-            const response = await api.post('carts/add/', {
-                'quantity':1,
-                'item':params } ) ;
-            return response.data;
-        } catch (error) {
-            throw new Error(error);
-        }
-    };
-
-    updateCart = async (cartItem,newCount) => {
-        try {
-            const response = await api.put('carts/update/'+ cartItem + '/' , {quantity:newCount} );
-            return response.data;
-        } catch (error) {
-            throw new Error(error);
-        }
-    };
-    deleteCart = async (cartItem) => {
-        try {
-            const response = await api.delete('carts/delete/'+ cartItem + '/' );
-            return response.data;
-        } catch (error) {
-            throw new Error(error);
-        }
-    };
-
-    orderAdd = async ({name, num, address, code,apt,city,state, subtotal}) => {
-        let user_id = JSON.parse(localStorage.getItem('LOGIN_USER_KEY')).id
-        try {
-            const response = await api.post('orders/add/', 
-
-    {
-        "total_price": subtotal,
-        "full_name":name,
-        "phone_number":num,
-        "address_line1": address,
-        "postal_code": code,
-        "address_line2": apt,
-        "city": city,
-        "user": user_id,
-        "state": "washington",
+  getCart = async () => {
+    try {
+      const response = await api.get('carts/');
+      return response.data;
+    } catch (error) {
+      throw new Error(error);
     }
-     ) ;
-            return response.data;
-        } catch (error) {
-            throw new Error(error);
+  };
+  addCart = async params => {
+    try {
+      const response = await api.post('carts/add/', {
+        'quantity': 1,
+        'item': params
+      });
+      return response.data;
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+
+  updateCart = async (cartItem, newCount) => {
+    try {
+      const response = await api.put('carts/update/' + cartItem + '/', { quantity: newCount });
+      return response.data;
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+  deleteCart = async (cartItem) => {
+    try {
+      const response = await api.delete('carts/delete/' + cartItem + '/');
+      return response.data;
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+
+  orderAdd = async ({ name, num, address, code, apt, city, state, subtotal }) => {
+    let user_id = JSON.parse(localStorage.getItem('LOGIN_USER_KEY')).id
+    try {
+      const response = await api.post('orders/add/',
+
+        {
+          "total_price": subtotal,
+          "full_name": name,
+          "phone_number": num,
+          "address_line1": address,
+          "postal_code": code,
+          "address_line2": apt,
+          "city": city,
+          "user": user_id,
+          "state": "washington",
         }
-    };  
+      );
+      return response.data;
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
 }
 
 
